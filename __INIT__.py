c2d5c46e54727e85a39f45887e0eed90e3243638
PomPomSqL <p0mp0mpuriiin.child@gmail.com>
2024-07-06 21:58:27 +0200

    Panel add

---
+++ __INIT__.py
@@ -0,0 +1,19 @@
+import bpy
+
+bl_info = {
+    "name": "Cutscene Engine SM64",
+    "blender": (4, 0 ,2),
+    "category": "Import/export",
+    "version": (0, 1),
+    "author": "IndigoSM64 & C0c0 San",
+    "description": "Cutscene Engine SM64 is a blender addon that add cutscene to SM64 ! WARNING: YOU MUST USE FAST64"
+}
+
+from .cutscene_trigger_exporter import cutscene_trigger_register
+from .cutscene_trigger_exporter import cutscene_trigger_unregister
+
+def register():
+    cutscene_trigger_register()
+
+def unregister():
+    cutscene_trigger_unregister()
---
+++ cutscene_trigger_exporter.py
@@ -0,0 +1,33 @@
+
+class SetupBoxEmpty(bpy.types.Operator):
+    bl_idname = "object.setup_box_empty"
+    bl_label = "Setup Box Empty"
+
+    def execute(self, context):
+        bpy.ops.object.empty_add(type='PLAIN_AXES')
+        obj = bpy.context.selected_objects[0]
+        obj.name = "cutscene_trigger"
+        return {'FINISHED'}
+
+class TriggerPanel(bpy.types.panel):
+
+    bl_idname = "TriggerPanel"
+    bl_label = "Cutscene Exporter"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Cutscene SM64"
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.operator("object.setup_box_empty", text="Add Box")
+        row = layout.row()
+        row.operator("object.setup_box_empty", text="Add Cylinder")
+
+    def cutscene_trigger_register():
+        bpy.utils.register_class(TriggerPanel)
+        bpy.utils.register_class(SetupBoxEmpty)
+
+    def cutscene_trigger_unregister():
+        bpy.utils.unregister_class(TriggerPanel)
+        bpy.utils.unregister_class(SetupBoxEmpty)
